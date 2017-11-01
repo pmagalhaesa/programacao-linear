db111664280143080aa6ebb46e236ff66a139a38
Paula Alves <pmagalhaesa@hotmail.com>
2017-11-01 20:32:36 -0200

    merge

---
+++ src/main/java/com/mycompany/programacao/linear/Resolucao.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.programacao.linear;
+import org.gnu.glpk.GLPK;
+import org.gnu.glpk.GLPKJNI;
+
+/**
+ *
+ * @author paula
+ */
+public class Resolucao {
+
+    /**
+     * Construtor da classe
+     */
+    public Resolucao(){}
+    
+    /**
+     * Retorna versão do GLPK
+     * @return string
+     */
+    
+    public static String getVersaoGLPK(){
+     // System.out.println(GLPK.GLP_ASN_MAX);
+        return GLPK.glp_version();
+    }
+    
+    
+}
